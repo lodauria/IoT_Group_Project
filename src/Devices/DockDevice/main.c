634d89911eaa8d452194ffde3a0f286a325ba35f
kernel-machine <l.giovannesi@gmail.com>
2021-05-15 16:32:11 +0200

    Monitor device created

--- src/Devices/DockDevice/main.c
+++ src/Devices/DockDevice/main.c
@@ -1,20 +1,27 @@
 #include <stdio.h>
 
 #include "xtimer.h"
+#include "periph/gpio.h"
 #include "devices/BoatPresenceEstimator.h"
+
 
 #define TRIGGER_PIN GPIO_PIN(PORT_B,3)  //D3
 #define ECHO_PIN GPIO_PIN(PORT_A,10)    //D2
+#define LED_PIN GPIO_PIN(PORT_B,5)      //D2
 
 boatPresenceEstimator_t boatPresenceEstimator;
+gpio_t led = LED_PIN;
 
 int main(void) {
     xtimer_init();
     printf("Hello from RIOT");
     boat_presence_estimator_init(&boatPresenceEstimator, TRIGGER_PIN, ECHO_PIN);
+    gpio_init(led, GPIO_OUT);
 
     while (1) {
-        printf("Is boat present %d\n", boat_presence_estimator_get_present(&boatPresenceEstimator,3));
+        int isPresent = boat_presence_estimator_get_present(&boatPresenceEstimator, 3);
+        printf("Is boat present %d\n", isPresent);
+        gpio_write(led, isPresent > 0 ? 1 : 0);
         xtimer_sleep(1);
     }
 
---
+++ src/Devices/MonitorDevice/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello from RIOT");
+
+    return 0;
+}
