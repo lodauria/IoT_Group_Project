3742a1657e669b6e1d03a9c777947e5e25a5a0b3
Luca Giovannesi <l.giovannesi@gmail.com>
2021-07-03 18:41:56 +0200

    GPS problem fix

--- src/WebPage/src/Pages/GPS.vue
+++ src/WebPage/src/Pages/GPS.vue
@@ -16,11 +16,11 @@
         </b-form-input>
       </b-form-group>
       <div class="my-2">
-        <b-button type="submit" variant="success" :disabled="watchId!=null">Enable</b-button>
-        <b-button class="mx-3" variant="danger" v-on:click="disableGPS" :disabled="watchId==null">Disable</b-button>
+        <b-button type="submit" variant="success" :disabled="sendingUpdate">Enable</b-button>
+        <b-button class="mx-3" variant="danger" v-on:click="disableGPS" :disabled="!sendingUpdate">Disable</b-button>
       </div>
     </b-form>
-    <div class="text-center" v-if="watchId!=null">
+    <div class="text-center" v-if="sendingUpdate">
       <h3 class="text-success">Sending device position...</h3>
       <p>Keep this page opened until the dock operation</p>
     </div>
@@ -38,6 +38,7 @@ export default {
         plate: '',
       },
       watchId: null,
+      sendingUpdate: false
     }
   },
   mounted() {
@@ -49,24 +50,35 @@ export default {
   },
   methods: {
     onSubmit(event) {
-      event.preventDefault();
+      event.preventDefault(); //Used to avoid page reload
+
       const options = {
         enableHighAccuracy: true,
         maximumAge: 1000
       }
-      this.watchId = navigator.geolocation.watchPosition(pos => {
-        this.sendGPSUpdate(pos.coords.latitude, pos.coords.longitude)
-      }, err => {
-        console.log(err.message)
-      }, options)
+
+      this.sendingUpdate = false
+
+      this.watchId = navigator.geolocation.watchPosition(
+          pos => {
+            this.sendGPSUpdate(pos.coords.latitude, pos.coords.longitude)
+          },
+          err => {
+            console.log(err.message)
+            this.sendingUpdate = false
+          },
+          options)
+
       return false;
     },
     disableGPS() {
       if (this.watchId) {
         navigator.geolocation.clearWatch(this.watchId);
         this.watchId = null
+        this.sendingUpdate = false
       }
     },
+
     sendGPSUpdate(lat, long) {
       console.log("POS", lat, long)
       const data = {}
@@ -78,6 +90,12 @@ export default {
         }
       }
       axios.post("https://kayib6g5eg.execute-api.us-east-1.amazonaws.com/default/GPSHandle", data, headers).then(d => {
+        if (d.data.ret_code != 0) { //If error
+          alert(d.data.info_mess)   //Show info_mess to the user
+          this.disableGPS();        //And disable GPS
+        } else {
+          this.sendingUpdate = true
+        }
         console.log(d.data)
       }).catch(e => {
         console.log(e)
@@ -88,7 +106,5 @@ export default {
 </script>
 
 <style scoped>
-.checkboxText {
-  padding-left: 10px;
-}
+
 </style>
